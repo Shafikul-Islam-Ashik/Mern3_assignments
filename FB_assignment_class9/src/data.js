0cd99f81460db055186f8227327032df4919478c
Shafiul Islam Ashik <98244474+Shafikul-Islam-Ashik@users.noreply.github.com>
2023-07-01 14:14:08 +0600

    Add files via upload

---
+++ FB_assignment_class9/src/data.js
@@ -0,0 +1,140 @@
+const posts = [
+  {
+    id: 1,
+    image: "./assets/images/post9.jpg",
+    content: `Cox's Bazar again.`,
+    likes: 700,
+    comments: 225,
+    timeAgo: "5 May",
+    author: {
+      userId: 9,
+      name: "Nazrul Islam Rakib",
+      photo: "./assets/images/user9.png",
+    },
+  },
+  {
+    id: 2,
+    image: "./assets/images/post10.jpg",
+    content: `Today's speial item.
+    Beef Burgur with vegetable.
+    Special discount for the first 100 customers.
+    Please inbox for order. 
+  `,
+    likes: 300,
+    comments: 25,
+    timeAgo: "21 Jun",
+    author: {
+      userId: 2,
+      name: "Shakib Molla",
+      photo: "./assets/images/user10.png",
+    },
+  },
+  {
+    id: 3,
+    image: "./assets/images/post3.jpg",
+    content: `Once upon a time......`,
+    likes: 50,
+    comments: 7,
+    timeAgo: "3 July",
+    author: {
+      userId: 3,
+      name: "Humaira Humaira",
+      photo: "./assets/images/user3.png",
+    },
+  },
+  {
+    id: 4,
+    image: "./assets/images/post4.jpg",
+    content: `My world,
+               I love my cuty`,
+    likes: 100,
+    comments: 26,
+    timeAgo: "8 d",
+    author: {
+      userId: 4,
+      name: "Neela Haque",
+      photo: "./assets/images/user4.png",
+    },
+  },
+  {
+    id: 5,
+    image: "./assets/images/post5.jpg",
+    content: `Kono kotha hobe na....`,
+    likes: 200,
+    comments: 125,
+    timeAgo: "4 d",
+    author: {
+      userId: 5,
+      name: "Ashik",
+      photo: "./assets/images/user5.png",
+    },
+  },
+  {
+    id: 6,
+    image: "./assets/images/post6.jpg",
+    content: `Aj amar mon valo nei..`,
+    likes: 570,
+    comments: 93,
+    timeAgo: "3 d",
+    author: {
+      userId: 6,
+      name: "Tamim Khandokar",
+      photo: "./assets/images/user6.png",
+    },
+  },
+  {
+    id: 7,
+    image: "./assets/images/post7.jpg",
+    content: `Today's special recepy. Inbox for order..`,
+    likes: 923,
+    comments: 577,
+    timeAgo: "1 h",
+    author: {
+      userId: 7,
+      name: "Priya Islam",
+      photo: "./assets/images/user7.png",
+    },
+  },
+  {
+    id: 8,
+    image: "./assets/images/post8.jpg",
+    content: `Onek din por try korlam. Just osadharon....`,
+    likes: 50,
+    comments: 11,
+    timeAgo: "30 min",
+    author: {
+      userId: 8,
+      name: "Sumaiya Simu",
+      photo: "./assets/images/user8.png",
+    },
+  },
+  {
+    id: 9,
+
+    image: "./assets/images/post2.jpg",
+    content: `Journey is always pleasure to me.
+    Let's enjoy our life...`,
+    likes: 550,
+    comments: 95,
+    timeAgo: "15 min",
+    author: {
+      userId: 1,
+      name: "Shafikul Islam",
+      photo: "./assets/images/user.png",
+    },
+  },
+  {
+    id: 10,
+    image: "./assets/images/post1.jpg",
+    content: "Kacchi bereani is favourite to me.",
+    likes: 980,
+    comments: 375,
+    timeAgo: "10 min",
+    author: {
+      userId: 10,
+      name: "Ashraful Haque",
+      photo: "./assets/images/users2.png",
+    },
+  },
+];
+// console.log(posts);
